a14050bab3cabeed4147928e5db879a2fbd58615
Daniel <daniel.naves@outlook.com>
2020-08-24 07:33:07 +0200

    Add rectangle struct

--- structs/struct.go
+++ structs/struct.go
@@ -1,5 +1,14 @@
 package structs
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
+}
+
+func Area(width, height float64) float64 {
+	return width * height
 }
