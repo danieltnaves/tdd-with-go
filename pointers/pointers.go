4c33923650583efa8abb44ec607882e2f0e847ea
Daniel <daniel.naves@outlook.com>
2020-08-30 17:50:29 +0200

    Pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,13 +1,39 @@
 package pointers
 
-type Wallet struct {
-	balance int
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
 }
